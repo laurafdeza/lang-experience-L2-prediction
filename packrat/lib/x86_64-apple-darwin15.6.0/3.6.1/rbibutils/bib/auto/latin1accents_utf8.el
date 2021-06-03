074d87ad4ebf2712f194ae6a4a9c86c3f92bdce6
Laura Fernández Arroyo <38184931+laurafdeza@users.noreply.github.com>
2021-06-03 13:45:32 -0400

    remove other exps

--- packrat/lib/x86_64-apple-darwin15.6.0/3.6.1/rbibutils/bib/auto/latin1accents_utf8.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "latin1accents_utf8"
- (lambda ()
-   (LaTeX-add-bibitems
-    "test1"))
- :bibtex)
-
--- packrat/lib/x86_64-apple-darwin15.6.0/3.6.1/report/NEWS.md
+++
@@ -1,5 +0,0 @@
-# report 0.3.0.9000 (development)
-
-# report 0.3.0
-
-* Initial release of the package.
